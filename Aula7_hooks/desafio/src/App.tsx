7bf10ea3681ea08c19f694523680a7c65d7acf99
Lucas Lacava <lucas.lacava@ab-inbev.com>
2020-10-14 12:23:12 -0300

    Desafio 7

--- Aula7_hooks/desafio/src/App.tsx
+++ Aula7_hooks/desafio/src/App.tsx
@@ -1,40 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-// import Animals from './apifake/Animals';
-// import Owners from './apifake/Owners';
+import Animals from './apifake/Animals';
+import Owners from './apifake/Owners';
+
+import IOwner from "./apifake/Interfaces/IOwner";
+import IAnimal from "./apifake/Interfaces/IAnimal";
+
 
 import "./style.css";
 
 // chamadas de api fake
-// const owners: Owners = new Owners();
-// const animals: Animals = new Animals();
+const ownersConstructor: Owners = new Owners();
+const animalsConstructor: Animals = new Animals();
 
-// owners.getAll(); // retorna promise
+
+
+
 // animals.getByOwnerId(); // retorna promise
 
 function App() {
+  const [owners, setOwners] = useState<IOwner[]>([]);
+  const [selectedOwnerId, setSelectedOwnerId] = useState(1)
+  const [animals, setAnimals] = useState<IAnimal[]>([]);
+  const [allAnimals, setAllAnimals] = useState({})
+
+
+  useEffect(() => {
+    ownersConstructor.getAll().then(ownersList => {
+      setOwners(ownersList)
+    })
+  }, [])
+
+  useEffect(() => {
+    animalsConstructor.getByOwnerId(Number(selectedOwnerId)).then(animal => setAnimals(animal))
+  }, [selectedOwnerId])
+
+
+  const handleSelectOnChange = (ownerId: string) => {
+    setSelectedOwnerId(Number(ownerId))
+  }
+
+  const renderOwners = () => (
+    owners.map(owner => {
+      return <option value={owner.id} key={owner.id}>{owner.name}</option>
+    })
+  )
+
+  const renderAnimals = () => (
+    animals.map(animal => {
+      return <option value={animal.id} key={animal.id}>{animal.name}</option>
+    })
+  )
+
+
+  const requestAllAnimals = () => {
+    const owner1 = animalsConstructor.getByOwnerId(1);
+    const owner2 = animalsConstructor.getByOwnerId(2);
+    const owner3 = animalsConstructor.getByOwnerId(3);
+    const owner4 = animalsConstructor.getByOwnerId(4);
+
+    Promise.all([owner1, owner2, owner3, owner4])
+      .then(([result0, result1, result2, result3]) => {
+        setAllAnimals({ [owners[0].name]: result0, [owners[1].name]: result1, [owners[2].name]: result2, [owners[3].name]: result3 })
+      })
+
+  }
+
   return (
     <div className="App">
       <section id="owners-section">
         <label htmlFor="owners">Donos:</label>
-        <select id="owners">
-          <option value="1">Dono 1</option>
-          <option value="2">Dono 2</option>
-          <option value="3">Dono 3</option>
-          <option value="4">Dono 4</option>
+        <select id="owners" onChange={event => handleSelectOnChange(event.target.value)}>
+          {renderOwners()}
         </select>
       </section>
       <section id="animals-section">
         <label htmlFor="animals">Animais:</label>
         <select id="animals">
-          <option>Animal 1</option>
-          <option>Animal 2</option>
-          <option>Animal 3</option>
-          <option>Animal 4</option>
+          {renderAnimals()}
         </select>
       </section>
       <section id="report">
-        <button>Ordenar donos com mais animais</button>
+        <button onClick={requestAllAnimals}>Ordenar donos com mais animais</button>
         <table id="reportList">
           <thead>
             <tr>
